7ae8692712978a387ff3f497c61fd0c424d95027
Ozark19 <55114864+Ozark19@users.noreply.github.com>
2020-03-26 17:29:43 -0300

    Updates

---
+++ precache-manifest.83195fc06e436b78cb08c136dd4d3d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c136e9e5a4c24e5ce3dc1ae409c01ff",
+    "url": "/sports/index.html"
+  },
+  {
+    "revision": "f34cb78314d6c7508d65",
+    "url": "/sports/static/css/main.9c37e7aa.chunk.css"
+  },
+  {
+    "revision": "7d884205492509a19472",
+    "url": "/sports/static/js/2.97b91625.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/sports/static/js/2.97b91625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34cb78314d6c7508d65",
+    "url": "/sports/static/js/main.8a915bb7.chunk.js"
+  },
+  {
+    "revision": "cbd7478faa05d53ff95d",
+    "url": "/sports/static/js/runtime-main.b677f3c3.js"
+  }
+]);
