5edeced47df40429a48ef3096ecee69ff7e76dbc
Ozark19 <55114864+Ozark19@users.noreply.github.com>
2020-05-27 11:00:32 -0300

    Updates

--- precache-manifest.f5118b590f274d2e956a88c71d6f971e.js
+++ precache-manifest.f5118b590f274d2e956a88c71d6f971e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a61858ed9c9c32f3d3bc5dc10bd823c5",
+    "revision": "c3646d0c8a86419f40758ee2ed487891",
     "url": "/sports/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sports/static/css/2.b90ce945.chunk.css"
   },
   {
-    "revision": "9573a96ebb4375058153",
+    "revision": "2ff20d77c179b3f36dd2",
     "url": "/sports/static/css/main.ff9b561b.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sports/static/js/2.0a45016c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9573a96ebb4375058153",
-    "url": "/sports/static/js/main.38693b84.chunk.js"
+    "revision": "2ff20d77c179b3f36dd2",
+    "url": "/sports/static/js/main.089e546f.chunk.js"
   },
   {
     "revision": "cbd7478faa05d53ff95d",
